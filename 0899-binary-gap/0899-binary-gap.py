36db351084a306faf6c97a30fece56f2f0ba41e1
haadi-gill <89543816+haadi-gill@users.noreply.github.com>
2024-07-16 23:19:25 -0400

    Time: 17 ms (45.87%), Space: 11.6 MB (22.94%) - LeetHub

---
+++ 0899-binary-gap/0899-binary-gap.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def binaryGap(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        max_count = 0
+        curr_count = 1
+        start_count = False
+
+        for i in [int(x) for x in bin(n)[2:]]:
+            if i :
+                if not start_count:
+                    start_count = True
+                else:
+                    if curr_count > max_count:
+                        max_count = curr_count
+                    curr_count = 1
+            elif start_count:
+                curr_count += 1
+                
+        return max_count
+        
