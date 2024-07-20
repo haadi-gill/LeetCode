350ef583f4d2439622a9bf27d2beba3f79836755
haadi-gill <89543816+haadi-gill@users.noreply.github.com>
2024-07-20 19:51:25 -0400

    Time: 807 ms (38.68%), Space: 27.1 MB (10.57%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+from math import comb
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        zeroRun = 0
+        d = defaultdict(int)
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                zeroRun += 1
+            else:
+                if zeroRun > 0: d[zeroRun] += 1
+                zeroRun = 0
+
+        if zeroRun > 0: d[zeroRun] += 1
+
+        total = 0
+        print(d)
+
+        for k,v in d.items():
+            for x in range(1,k+1):
+                total += (k- x +1) * v
+
+        return total
