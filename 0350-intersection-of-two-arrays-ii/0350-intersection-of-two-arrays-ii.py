e0bdb91e72e3f2e93f88f32312ca8c1effc38666
haadi-gill <89543816+haadi-gill@users.noreply.github.com>
2024-07-22 23:20:50 -0400

    Time: 36 ms (28.55%), Space: 11.9 MB (16.72%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -8,11 +8,7 @@ class Solution(object):
         a = Counter(nums1)#.most_common()
         b = Counter(nums2)#.most_common()
 
-        if len(a) > len(b):
-            a,b = b,a
-
-        # print(a)
-        # print(b)
+       
 
         c = []
 
