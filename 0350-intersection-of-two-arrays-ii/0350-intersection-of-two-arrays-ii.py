1f2d21d125273199755652b8a209f9356f852845
haadi-gill <89543816+haadi-gill@users.noreply.github.com>
2024-07-22 23:18:36 -0400

    Time: 22 ms (92.53%), Space: 12.1 MB (16.72%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        a = Counter(nums1)#.most_common()
+        b = Counter(nums2)#.most_common()
+
+        if len(a) > len(b):
+            a,b = b,a
+
+        # print(a)
+        # print(b)
+
+        c = []
+
+        for i in list(set(a).intersection(set(b))):
+            c.extend([i]*min(a[i], b[i]))
+
+        return c
