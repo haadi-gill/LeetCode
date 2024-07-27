9221f82e2857edcd8bdbc16679be77fbaec597ba
haadi-gill <89543816+haadi-gill@users.noreply.github.com>
2024-07-27 13:54:24 -0400

    Time: 80 ms (34.83%), Space: 47.1 MB (24.72%) - LeetHub

---
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int BuyChoco(int[] prices, int money) {
+        int minMoney = -1;
+        int n = prices.Length;
+
+        for (int i = 0; i < n-1; i++){
+            for (int j = i+1; j < n; j++){
+                int diff = money - prices[i] - prices [j];
+                if (diff >= 0 && diff > minMoney){
+                    minMoney = diff;
+                }
+            }
+        }
+
+        if (minMoney == -1){
+            return money;
+        } 
+        else{
+            return minMoney;
+        }
+    }
+}
